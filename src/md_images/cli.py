53893f7f30ab2d067509791ee01f3ac6adc006dc
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2024-11-10 18:21:06 +0100

    new cli

---
+++ src/md_images/cli.py
@@ -0,0 +1,207 @@
+from pathlib import Path
+from typing import Annotated, Literal
+from panflute import (
+    BulletList,
+    Doc,
+    Header,
+    Inline,
+    Link,
+    ListItem,
+    Plain,
+    Str,
+    convert_text,
+    stringify,
+)
+from rich import print
+from rich.syntax import Syntax
+
+from cyclopts import App, Parameter
+
+from md_images.core import relative_fspath
+
+from .model import MdFile, SourceSelection
+from .core import find_all
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+app = App(default_parameter=Parameter(negative=[]), help_format="rst")
+
+
+Texts = Annotated[
+    list[Path],
+    Parameter(
+        negative=[],
+        help="Text file(s) to analyze. May be anything pandoc can read.",
+    ),
+]
+
+Select = Annotated[
+    SourceSelection, Parameter(["-s", "--select"], help=SourceSelection.__doc__)
+]
+
+
+@app.command
+def ls(texts: Texts, /, *, select: Select = SourceSelection.EXPLICIT):
+    """List image files included in the given text files"""
+    all_images = set()
+    for text in texts:
+        source = MdFile(text)
+        all_images.update(source.image_sources(select))
+    print("\n".join(relative_fspath(img) for img in all_images))
+
+
+@app.command
+def dep(
+    texts: Texts,
+    /,
+    *,
+    suffix: Annotated[
+        list[str] | None,
+        Parameter(
+            [
+                "-d",
+                "--suffix",
+            ]
+        ),
+    ] = None,
+    individual_dependencies: Annotated[
+        str | None, Parameter(["-i", "--individual-dependencies"])
+    ] = None,
+):
+    """
+    Print makefile rules for the given text files.
+
+    Args:
+        texts: file or files to analyze. Can actually be anything pandoc is able to read, not only markdown files.
+        suffix: suffix for the target rules in the makefile. If you provide multiple suffixes separated by space, a rule will be created for each suffix. You can also provide a pattern using '%'
+        individual_dependencies: if provided, write an individual dependenca file with the given suffix for each source file
+    """
+    for text in texts:
+        source = MdFile(text)
+        rules = "\n".join([source.rule(suf) for suf in suffix or []] or [source.rule()])
+        if individual_dependencies:
+            text.with_suffix(individual_dependencies).write_text(rules + "\n")
+        else:
+            print(rules)
+
+
+@app.command
+def cp(
+    texts: Texts,
+    target: Path,
+    /,
+    select: Select = SourceSelection.SOURCE,
+):
+    """
+    Copy text files including linked image files to the given target.
+
+    If a single source file is provided and the target is not an existing directory,
+    it is interpreted as the target file name, otherwise it is thought to be a directory.
+    All neccessery directories are created if they do not exist.
+    """
+    if target.is_dir():
+        target_dir = target
+    elif len(texts) > 1 and not target.exists():
+        target_dir = target
+        if target.suffixes:
+            logger.warning(
+                "Since multiple sources are provided, %s is created as a directory",
+                target_dir,
+            )
+    else:
+        target_dir = target.parent
+
+    target_dir.mkdir(parents=True, exist_ok=True)
+    for text in texts:
+        source = MdFile(text)
+        if target_dir == target:
+            dest = target_dir / text.name
+        else:
+            dest = target
+        source.copy(dest, select)
+
+
+@app.command
+def links(
+    texts: Texts,
+    /,
+    format: Annotated[
+        Literal["tabbed", "url"] | str, Parameter(["-f", "--format"])
+    ] = "tabbed",
+):
+    """
+    List all links in the given text file.
+
+    Args:
+        format: output format. "tabbed" (default) generates a TSV table
+                of source, URL and title, "url" generates a list of URLs only.
+                Additionally, you can pass any format pandoc is able to
+                generate.
+    """
+    result = []
+    for text in texts:
+        doc = MdFile(text).doc
+        title = doc.get_metadata("title") or text.stem
+        links: list[Link] = find_all(doc, Link)  # type: ignore
+        if format == "url":
+            result.extend(link.url for link in links)
+        elif format == "tabbed":
+            result.extend(
+                "\t".join([str(text), link.url, stringify(link)]) for link in links
+            )
+        else:
+            items = [ListItem(Plain(link)) for link in links]
+            bullet_list = BulletList()
+            bullet_list.content.extend(items)
+            result.append(Header(Link(Str(title), url=str(text)), level=2))
+            result.append(bullet_list)
+
+    if format == "tabbed" or format == "url":
+        print("\n".join(result))
+    else:
+        print(
+            Syntax(
+                convert_text(result, input_format="panflute", output_format=format),
+                format,
+            )
+        )
+
+
+@app.default
+def md_images(
+    markdown: list[Path],
+    /,
+    *,
+    list_: Annotated[bool, Parameter(["-l", "--list"], negative=[])] = False,
+    suffix: Annotated[
+        list[str] | None, Parameter(["-d", "--suffix"], negative=[])
+    ] = None,
+    individual_dependencies: Annotated[
+        str | None, Parameter(["-i", "--individual-dependencies"])
+    ] = None,
+):
+    """
+    Analyze the listed markdown files for images.
+
+    Args:
+        markdown: file or files to analyze. Can actually be anything pandoc is able to read, not only markdown files.
+        suffix: suffix for the target rules in the makefile. If you provide multiple suffixes, a rule will be created for each suffix.
+
+    """
+    all_dependencies = set()
+    for source_file in markdown:
+        source = MdFile(source_file)
+        if list:
+            all_dependencies.update(source.image_paths)
+        else:
+            rules = "\n".join(
+                [source.rule(suf) for suf in suffix or []] or [source.rule()]
+            )
+            if individual_dependencies:
+                source_file.with_suffix(individual_dependencies).write_text(
+                    rules + "\n"
+                )
+            else:
+                print(rules)
--- src/md_images/model.py
+++ src/md_images/model.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from functools import cached_property
 from pathlib import Path
 
@@ -5,6 +6,20 @@ from .core import find_images, load_markdown, relative_fspath, resolve_url
 from .prefer_variants import rank_variants
 from typing import Callable
 from shutil import copy2
+
+
+class SourceSelection(Enum):
+    """
+    If *explicit*, only include image files listed in the text file.
+    If *source*, look for a file with preferred extensions but the same
+    name and directory as the listed file and include that instead,
+    if it exists. If *both*, include both.
+    """
+
+    EXPLICIT = "explicit"
+    SOURCE = "source"
+    BOTH = "both"
+    ALL = "all"
 
 
 class MdFile:
@@ -22,9 +37,22 @@ class MdFile:
         resolved = [resolve_url(img, self.path) for img in self.image_urls]
         return {path for path in resolved if isinstance(path, Path)}
 
-    def image_sources(self, ranker: Callable[[Path], int] | None = None) -> set[Path]:
-        ranked = rank_variants(self.image_paths, find_variants=True, ranker=ranker)
-        return {variants[0] for variants in ranked.values()}
+    def image_sources(
+        self,
+        selection: SourceSelection = SourceSelection.SOURCE,
+        ranker: Callable[[Path], int] | None = None,
+    ) -> set[Path]:
+        result = set()
+        if selection == SourceSelection.EXPLICIT or selection == SourceSelection.BOTH:
+            result |= self.image_paths
+        if selection != SourceSelection.EXPLICIT:
+            ranked = rank_variants(self.image_paths, find_variants=True, ranker=ranker)
+            if selection == SourceSelection.SOURCE or selection == SourceSelection.BOTH:
+                result |= {variants[0] for variants in ranked.values()}
+            elif selection == SourceSelection.ALL:
+                for variants in ranked.values():
+                    result.update(variants)
+        return result
 
     def rule(self, suffix: str | None = None, base: Path = Path()) -> str:
         if suffix is None:
@@ -40,22 +68,17 @@ class MdFile:
 
         return f"{relative_fspath(target, base)} : {' '.join(relative_fspath(dep, base) for dep in deps)}"
 
-    def copy(self, target: Path, sources=True, generated=False) -> None:
+    def copy(self, target: Path, selection: SourceSelection = SourceSelection.SOURCE):
         if target.is_dir():
             target_dir = target
             doc_target = target / self.path.name
         else:
             target_dir = target.parent
             doc_target = target
-        img_sources = set()
-        if sources:
-            img_sources.update(self.image_sources())
-        if generated:
-            img_sources.update(self.image_paths)
 
         target_dir.mkdir(parents=True, exist_ok=True)
         copy2(self.path, doc_target)
-        for img in img_sources:
+        for img in self.image_sources(selection):
             dest = target_dir / img.relative_to(self.path.parent)
             dest.parent.mkdir(parents=True, exist_ok=True)
             copy2(img, dest)
